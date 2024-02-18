01b5d05ebd3a544684501d5504a442177b761571
Kevin Tran <kthightran024@gmail.com>
2024-02-18 13:44:18 -0700

    Created main files and JS skeleton

---
+++ index.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  return outcome;
+}
+
+function playGame() {
+  for (let i = 0; i=5; i++) {
+    playRound();
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
