f0fe743402e96773ae606e1ff9b14c6d691b363d
Kevin Tran <kthightran024@gmail.com>
2024-05-14 18:17:05 -0700

    added score counter, added finished game functions

--- index.js
+++ index.js
@@ -13,63 +13,60 @@ function getComputerChoice() {
 	return computerChoice;
 }
 
-rockEl.addEventListener('click', () => {playRound('Rock')});
-paperEl.addEventListener('click', () => {playRound('Paper')});
-scissorsEl.addEventListener('click', () => {playRound('Scissors')});
+let playerScore = 0;
+let computerScore = 0;
+
+rockEl.addEventListener('click', () => {
+	playRound('Rock');
+});
+paperEl.addEventListener('click', () => {
+	playRound('Paper');
+});
+scissorsEl.addEventListener('click', () => {
+	playRound('Scissors');
+});
 
 function playRound(playerSelection) {
-  let computerSelection = getComputerChoice();
-  let result = checkWinner(playerSelection, computerSelection);
-  if (result == 'Tie') {
-    resultsEL.textContent = `This round is a tie! The Computer also chose ${playerSelection}`;
-  } else if (result == 'Player') {
-    resultsEL.textContent = `You won this round! ${playerSelection} (Your Move) beats ${computerSelection} (Computer's Move)`;
-    playerScoreEl.textContent = 
-  } else {
-    resultsEL.textContent = `You lost this round! ${computerSelection} (Computer's Move) beats ${playerSelection} (Your Move)`;
+	let computerSelection = getComputerChoice();
+	let result = checkWinner(playerSelection, computerSelection);
+	if (result == 'Tie') {
+		resultsEL.textContent = `This round is a tie! The Computer also chose ${playerSelection}`;
+	} else if (result == 'Player') {
+		resultsEL.textContent = `You won this round! ${playerSelection} (Your Move) beats ${computerSelection} (Computer's Move)`;
+    playerScore++;
+	} else {
+		resultsEL.textContent = `You lost this round! ${computerSelection} (Computer's Move) beats ${playerSelection} (Your Move)`;
+    computerScore++;
+	}
+	playerScoreEl.textContent = `Player: ${playerScore}`;
+	computerScoreEl.textContent = `Computer: ${computerScore}`;
+  scoreChecker(playerScore, computerScore);
+}
+
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return 'Tie';
+	} else if (
+    (playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+		(playerSelection == 'Paper' && computerSelection == 'Rock') ||
+		(playerSelection == 'Scissors' && computerSelection == 'Paper')
+	) {
+    return 'Player';
+	} else {
+    return 'Computer';
+	}
+}
+
+function scoreChecker(playerScore, computerScore) {
+  if (playerScore === 5) {
+    resultsEL.textContent = "You won the game! Refresh browser to play again";
+    rockEl.disabled = true;
+    paperEl.disabled = true;
+    scissorsEl.disabled = true;
+  } else if (computerScore === 5) {
+    resultsEL.textContent = 'You lost the game! Refresh browser to play again';
+    rockEl.disabled = true;
+    paperEl.disabled = true;
+    scissorsEl.disabled = true;
   }
 }
-
-function playGame() {
-	let playerScore = 0;
-	let computerScore = 0;
-	for (let i = 0; i < 5; i++) {
-		let computerSelection = getComputerChoice();
-		if (checkWinner(playerSelection, computerSelection) == 'Player') {
-			playerScore++;
-		} else if (checkWinner(playerSelection, computerSelection) == 'Computer') {
-			computerScore++;
-		}
-	}
-}
-
-
-	function checkWinner(playerSelection, computerSelection) {
-		if (playerSelection == computerSelection) {
-			return 'Tie';
-		} else if (
-			(playerSelection == 'Rock' && computerSelection == 'Scissors') ||
-			(playerSelection == 'Paper' && computerSelection == 'Rock') ||
-			(playerSelection == 'Scissors' && computerSelection == 'Paper')
-		) {
-			return 'Player';
-		} else {
-			return 'Computer';
-		}
-	}
-
-	// if (playerScore > computerScore) {
-	// 	resultsEL.textContent = 'The game is over...Luckily, you won the game! Thanks for Playing!';
-	// } else if (computerScore > playerScore) {
-	// 	resultsEL.textContent = 'The game is over...Unfortunately, you lost the game :( Thanks for Playing!';
-	// } else {
-	// 	resultsEL.textContent = 'The game is over...Surprisingly, this game is a tie! Thanks for Playing!';
-	// }
-
-// function getPlayerChoice() {
-// 	let initialChoice = prompt('Choose Rock, Paper, or Scissors'); // Initial choice that user inputs, no styling
-// 	const LOWER_CASE = initialChoice.slice(1, initialChoice.length).toLowerCase(); // Makes all letters except for first letter lower case
-// 	const UPPER_1ST_CHAR = initialChoice.charAt(0).toUpperCase(); // Makes first letter upper case
-// 	const PLAYER_CHOICE = UPPER_1ST_CHAR.concat(LOWER_CASE); // Combines upper case letter and rest of lower case letters into 1 string
-// 	return PLAYER_CHOICE; // returns complete string
-// }
